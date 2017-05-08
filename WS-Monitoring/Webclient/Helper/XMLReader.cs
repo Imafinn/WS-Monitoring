652f0cba091a5a1f18fcf49533904673c14a77a5
BUCHER Mike <mike.bucher@axa-winterthur.ch>
2017-05-08 11:22:52 +0200

    added xmlreader

---
+++ WS-Monitoring/Webclient/Helper/XMLReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+using Webclient.Models;
+
+namespace Webclient.Helper
+{
+    public class XMLReader
+    {
+        static List<ServiceExtended> readServices()
+        {
+            List<ServiceExtended> items = new List<ServiceExtended>();
+            using (var reader = new StreamReader("C:/test/text.xml"))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(List<ServiceExtended>),
+                    new XmlRootAttribute("services"));
+                items = (List<ServiceExtended>)deserializer.Deserialize(reader);
+            }
+
+            return items;
+        }
+    }
+}
--- WS-Monitoring/Webclient/Models/ServiceExtended.cs
+++ WS-Monitoring/Webclient/Models/ServiceExtended.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace Webclient.Models
 {
     public class ServiceExtended
     {
+        [XmlElement("id")]
         public int Id { get; set; }
+        [XmlElement("name")]
         public string Name { get; set; }
+        [XmlElement("description")]
         public string Description { get; set; }
+        [XmlElement("status")]
         public string Status { get; set; }
     }
 }
