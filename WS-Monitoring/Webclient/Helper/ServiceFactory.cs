c86eb7d09b1caaa5881ae148de80d971443aaa79
Holyoneqt <spuehler.luca@gmail.com>
2017-05-08 15:37:25 +0200

    ServiceFactory added

---
+++ WS-Monitoring/Webclient/Helper/ServiceFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace Webclient.Helper
+{
+	public class ServiceFactory
+	{
+        public ServiceFactory()
+        {
+
+        }
+
+        public ServiceController GetService(int id)
+        {
+            string processName = Process.GetProcessById(id).ProcessName;
+            List<ServiceController> matchingServices = ServiceController.GetServices()
+                                                                        .Where(sc => sc.ServiceName.Equals(processName))
+                                                                        .ToList();
+
+            if(matchingServices.Count == 1)
+            {
+                return matchingServices[0];
+            }
+            else if(matchingServices.Count == 0)
+            {
+                throw new ArgumentException($"No Services found with the ID '{id}'");
+            }
+            else
+            {
+                throw new ArgumentException($"Found more than one Service with the ID '{id}'");
+            }
+        }
+	}
+}
