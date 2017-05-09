025333761ea8cf95dd901a6535e1bba1bad91147
FREI Sven <sven.frei@axa-winterthur.com>
2017-05-09 09:34:03 +0200

    Documentation in MVC completed.

--- WS-Monitoring/Webclient/Helper/ServiceRepo.cs
+++ WS-Monitoring/Webclient/Helper/ServiceRepo.cs
@@ -7,11 +7,23 @@ using Webclient.Models;
 
 namespace Webclient.Helper
 {
+    /// <summary>
+    /// ServiceRepo executes the different actions with services.
+    /// </summary>
     public class ServiceRepo : IServiceRepo
     {
+        /// <summary>
+        /// List received by the XMLReader, it contains the basic services.
+        /// </summary>
         private List<ServiceExtended> _servicesBasic;
-        //private List<ServiceExtended> _servicesExtended;
+        /// <summary>
+        /// List received by the ServiceController, it contains the extended services.
+        /// </summary>
+        private List<ServiceExtended> _servicesExtended;
 
+        /// <summary>
+        /// In the Constructor the servicelists get initalized.
+        /// </summary>
         public ServiceRepo()
         {
             _servicesBasic = XMLReader.ReadServices();
