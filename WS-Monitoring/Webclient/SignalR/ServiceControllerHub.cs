3f001bbe5736181ee1fed424171a0f54f7f68673
Luca Spühler <spuehler.luca@gmail.com>
2017-05-10 09:59:39 +0200

    Backend for signalR implemented

---
+++ WS-Monitoring/Webclient/SignalR/ServiceControllerHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.ServiceProcess;
+
+namespace Webclient.SignalR
+{
+    public class ServiceControllerHub : Hub
+    {
+        public void UpdateService(ServiceController service)
+        {
+            Clients.All.onServerUpdate(service);
+        }
+    }
+}
---
+++ WS-Monitoring/Webclient/SignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Webclient.SignalR.Startup))]
+namespace Webclient.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
