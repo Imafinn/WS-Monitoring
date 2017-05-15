dc674dd8b3eb707b9390d3e28052eec8744ab3ca
BUCHER Mike <mike.bucher@axa-winterthur.ch>
2017-05-15 13:08:28 +0200

    added performance timer

---
+++ WS-Monitoring/Webclient/Helper/IPerformanceTimer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+using Webclient.Models;
+
+namespace Webclient.Helper
+{
+    public interface IPerformanceTimer
+    {
+        void TimerInit();
+
+        void OnTickEvent(object source, ElapsedEventArgs e);
+    }
+}
---
+++ WS-Monitoring/Webclient/Helper/PerformanceTimer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Timers;
+using System.Web;
+using Webclient.Models;
+
+namespace Webclient.Helper
+{
+    public class PerformanceTimer : IPerformanceTimer
+    {
+
+        public PerformanceTimer(List<ServiceFull> list)
+        {
+            ServiceList = list;
+            TimerInit();
+        }
+
+        public void OnTickEvent(object source, ElapsedEventArgs e)
+        {
+            foreach (ServiceFull service in ServiceList)
+            {
+                string query = $"SELECT ProcessId FROM Win32_Service WHERE Name = '{service.ServiceName}'";
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher(query);
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    int processId = Int32.Parse(obj["processId"].ToString());
+                    Process process = null;
+                    try
+                    {
+                        process = Process.GetProcessById((int)processId);
+                    }
+                    catch (ArgumentException)
+                    {
+                        // Thrown if the process specified by processId
+                        // is no longer running.
+                    }
+                    try
+                    {
+                        if (process != null)
+                        {
+                            PerformanceCounter myAppCpu = new PerformanceCounter(
+                                "Process", "% Processor Time", process.ProcessName, true);
+                            PerformanceCounter myAppRam = new PerformanceCounter(
+                                "Process", "Working Set - Private", process.ProcessName, true);
+                            myAppCpu.NextValue();
+                            service.PerformanceRAM = (myAppRam.NextValue() / (int)(1024)).ToString();
+                            Thread.Sleep(1000);
+                            service.PerformanceCPU = myAppCpu.NextValue().ToString();
+                        }
+                    }
+                    catch (Win32Exception)
+                    {
+                        // Thrown if process is already terminating,
+                        // the process is a Win16 exe or the process
+                        // could not be terminated.
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Thrown if the process has already terminated.
+                    }
+                }
+            }
+        }
+
+        public void TimerInit()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTickEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+        }
+
+        List<ServiceFull> ServiceList { get; set; }
+    }
+}
